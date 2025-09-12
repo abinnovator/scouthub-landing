f08276927aaf456a1f61307514ac696d821f6a1f
Abinnovator <abtheinnovator@gmail.com>
2025-09-12 11:34:21 +0530

    add hero section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-between  border-2 border-black rounded-3xl py-5 px-5 items-center ">
-      Navbar
+      {/* <Image
+        src="/logo_size.jpg"
+        alt="scouthub logo"
+        width={111}
+        height={190}
+      /> */}
+      <p>ScoutHub</p>
+
       <div className="flex flex-row gap-10">
         <Link href="#talent">Showcase Talent</Link>
         <Link href="#faq">FAQ</Link>
---
+++ components/sections/Hero.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="md:flex md:flex-row flex flex-col gap-64">
+      <div className="flex flex-col pt-48 gap-5">
+        <Badge variant="default" className="bg-[#7A8CF3]">
+          Trusted by 10,000+ Athletes
+        </Badge>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-5xl font-semibold">Get Scouted,Go Pro</h1>
+          <p className="max-w-[418px]">
+            Upload your gameplay footage and connect with coaches and scouts
+            from professional teams. Your talent deserves to be seen.
+          </p>
+          <div className="flex flex-row gap-8">
+            <Button className="bg-[#7A8CF3] border-[#5B73F6] border-4 cursor-pointer">
+              <Link href="https://scouthub.appwrite.network">
+                Start Your Journey
+              </Link>
+            </Button>
+            <Button className="bg-white text-black cursor-pointer border-[#5B73F6] border-4">
+              <Link href="https://scouthub.appwrite.network">Watch Demo</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Image
+        src="/Rectangle 1 (1).png"
+        alt="football img"
+        width={591}
+        height={494}
+        className="pt-24"
+      />
+    </div>
+  );
+};
+
+export default Hero;
