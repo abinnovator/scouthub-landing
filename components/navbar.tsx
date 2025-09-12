75f060585ca428f51392fa925f766bdd9f073f03
Abinnovator <abtheinnovator@gmail.com>
2025-09-12 10:50:23 +0530

    sim;ple layout with navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row justify-between  border-2 border-black rounded-3xl py-5 px-5 items-center ">
+      Navbar
+      <div className="flex flex-row gap-10">
+        <Link href="#talent">Showcase Talent</Link>
+        <Link href="#faq">FAQ</Link>
+      </div>
+      <div className="flex flex-row gap-3">
+        <Button className="bg-gradient-to-b from-[#EBF3FA] to-[#DFEDFA] text-black shadow-md cursor-pointer">
+          Sign Up
+        </Button>
+        <Button className="bg-gradient-to-b from-[#EBF3FA] to-[#DFEDFA] text-black shadow-md cursor-pointer">
+          Sign In
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
